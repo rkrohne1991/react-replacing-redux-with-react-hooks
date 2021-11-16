eeace49a65dc9704d2a2b6e0057b74cf4dfce228
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-11-16 07:56:43 +0000

    Custom Hook as a Store

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import ProductsProvider from "./context/products-context";
+import configureProductStore from "./hooks-store/products-store";
+
+configureProductStore();
 
 ReactDOM.render(
-  <ProductsProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ProductsProvider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 );
